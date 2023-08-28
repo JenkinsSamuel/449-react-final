f488b04531696977ca233d84e8ee6a3858a6da85
Sam Jenkins <samjenkins@Sams-MacBook-Pro-2.local>
2023-08-28 19:02:09 -0400

    removed library

--- src/App.js
+++ src/App.js
@@ -3,44 +3,10 @@ import axios from 'axios'
 
 
 
-import { createClient } from "@supabase/supabase-js"
-
-const supabaseUrl = 'https://ndmepqcoazrhdeaiimjh.supabase.co'
-
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5kbWVwcWNvYXpyaGRlYWlpbWpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc1NTU0OTksImV4cCI6MTk5MzEzMTQ5OX0.uULbqkCaqecExdYEY_b_jCgMxvyyJrGQQ5mGFzcFVOk'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
 
 
-// A React component that queries and displays data from Supabase
-function Library() {
-  // The useState hook lets us store data in a component across renders
-  // setMyBooks is a setter function that updates the state of myBooks
-  const [myBooks, setMyBooks] = useState([]);
-  // This should look familar from Codepen
-  async function getBooks() {
-    let { data: books} = await supabase
-      .from('books')
-      .select('*')
-    // Update the state
-    setMyBooks(books);
-  }
-  // Execute the function
-  getBooks();
-  // Below is what displays when you use <Library />
-  return (
-    <table>
-    {
-      myBooks.map(b => (
-        <tr>
-          <td>{b.cities}</td>
-          <td>{b.states}</td>
-        </tr>
-      ))
-    }
-    </table>
-  )
-}
+
+
 
 
 
